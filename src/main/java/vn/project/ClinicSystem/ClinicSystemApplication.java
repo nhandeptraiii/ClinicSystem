6b6dd5927291c18d4bc33eddfbf9dafba3ba2388
nhandeptraiii <thanhnhan21082003@gmail.com>
2025-09-15 11:49:13 +0700

    create project

---
+++ src/main/java/vn/project/ClinicSystem/ClinicSystemApplication.java
@@ -0,0 +1,13 @@
+package vn.project.ClinicSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicSystemApplication.class, args);
+	}
+
+}
