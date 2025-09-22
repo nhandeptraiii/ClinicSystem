4c5c2cd0b52564d9432b2fe1e65cfb12b4dd0719
nhandeptraiii <thanhnhan21082003@gmail.com>
2025-09-22 21:44:04 +0700

    format datetime theo dang iso8601, thêm lombok

---
+++ Backend/src/main/java/vn/project/ClinicSystem/config/DateTimeFormatConfiguration.java
@@ -0,0 +1,18 @@
+package vn.project.ClinicSystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Enable ISO 8601 date format globally
+@Configuration
+public class DateTimeFormatConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setUseIsoFormat(true);
+        registrar.registerFormatters(registry);
+    }
+}
