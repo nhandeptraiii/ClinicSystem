583e09495ecfdbdc6dd546bb099abe04974ae76e
nhandeptraiii <thanhnhan21082003@gmail.com>
2025-10-14 18:22:53 +0700

    xóa phần không sử dụng

--- Backend/src/main/java/vn/project/ClinicSystem/model/dto/ServiceIndicatorCreateRequest.java
+++ Backend/src/main/java/vn/project/ClinicSystem/model/dto/ServiceIndicatorCreateRequest.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
