fd5048976abd99b1ee862a33e6f2992669171b33
nhandeptraiii <thanhnhan21082003@gmail.com>
2025-11-06 15:02:49 +0700

    config(BE): cho phép gửi dạng Patch

--- Backend/src/main/java/vn/project/ClinicSystem/config/CorsConfig.java
+++ Backend/src/main/java/vn/project/ClinicSystem/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
